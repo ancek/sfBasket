faee0b71924b9bef0c720ca22cd890bf3cd550b7
Jakub Anckiewicz <jakub.anckiewicz@gmail.com>
2015-10-18 15:49:20 +0200

    Dodanie listy produktów

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -6,23 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class ProductController extends Controller {
+class ProductController extends Controller
+{
 
     /**
-     * @Route("/list")
-     * @Template()
+     * @Route("/list", name="product_list")
      */
-    public function listAction() {
-        return array(
-                // ...
-        );
+    public function listAction()
+    {
+        $products = $this->getProducts();
+        
+        return $this->render('product/list.html.twig', [
+            'products' => $products
+        ]);
     }
 
     /**
      * @Route("/{id}/add-to-cart")
      * @Template()
      */
-    public function addToCartAction($id) {
+    public function addToCartAction($id)
+    {
         return array(
                 // ...
         );
@@ -32,7 +36,8 @@ class ProductController extends Controller {
      * @Route("/basket")
      * @Template()
      */
-    public function basketAction() {
+    public function basketAction()
+    {
         return array(
                 // ...
         );
@@ -42,7 +47,8 @@ class ProductController extends Controller {
      * @Route("/{id}/remove-from-cart")
      * @Template()
      */
-    public function removeFromCartAction($id) {
+    public function removeFromCartAction($id)
+    {
         return array(
                 // ...
         );
@@ -52,10 +58,28 @@ class ProductController extends Controller {
      * @Route("/clearBasket")
      * @Template()
      */
-    public function clearBasketAction() {
+    public function clearBasketAction()
+    {
         return array(
                 // ...
         );
+    }
+    
+    private function getProducts()
+    {
+        $file = file('product.txt');
+        $products = [];
+        foreach ($file as $p) {
+            $e = explode(':', trim($p));
+            $products[$e[0]] = array(
+                'id' => $e[0],
+                'name' => $e[1],
+                'price' => $e[2],
+                'description' => $e[3],
+            );
+        }
+
+        return $products;
     }
 
 }
