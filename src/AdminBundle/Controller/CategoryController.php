92b10f4d8d3d656d0b180fd3074dcd09e98dddf3
Jakub Anckiewicz <jakub.anckiewicz@gmail.com>
2015-11-08 11:35:50 +0100

    Stworzenie Admina

---
+++ src/AdminBundle/Controller/CategoryController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * @Route("/category")
+ */
+class CategoryController extends Controller
+{
+
+    /**
+     * @Route("/list")
+     * @Template()
+     */
+    public function listAction()
+    {
+        return array(
+                // ...
+        );
+    }
+
+    /**
+     * @Route("/new")
+     * @Template()
+     */
+    public function newAction()
+    {
+        return array(
+                // ...
+        );
+    }
+
+    /**
+     * @Route("/edit/{id}")
+     * @Template()
+     */
+    public function editAction($id)
+    {
+        return array(
+                // ...
+        );
+    }
+
+    /**
+     * @Route("/delete/{id}")
+     * @Template()
+     */
+    public function deleteAction($id)
+    {
+        return array(
+                // ...
+        );
+    }
+
+}
